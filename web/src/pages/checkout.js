01bdc4fbc27e1f5b3449a7d96cfe7e975de50bc3
Andre Parsons <andrparsons@gmail.com>
2021-10-23 15:01:26 -0400

    fix to form legibility

--- web/src/pages/checkout.js
+++ web/src/pages/checkout.js
@@ -1,13 +1,20 @@
 import * as React from "react"
+import { createGlobalStyle } from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import CheckoutForm from "../components/checkoutForm"
-import Summary from "../components/checkoutSummary"
+
+const GlobalStyleCheckout = createGlobalStyle`
+  body {
+    background-color: var(--almostWhite);
+  }
+`
 
 export default function Checkout() {
   return (
     <Layout noAbout>
+      <GlobalStyleCheckout />
       <Seo title="Checkout" />
       <CheckoutForm />
     </Layout>
